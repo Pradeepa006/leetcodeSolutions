8104077da13a9e4cc6d429a92d71253e5d3e4ccb
Pradeepa006 <155996905+Pradeepa006@users.noreply.github.com>
2025-06-12 15:07:51 +0530

    Time: 6 ms (45.25%), Space: 42.8 MB (94.14%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-       if(s.length() != t.length())
+        if(s.length() != t.length())
            return false;
         
         int[] freq = new int[26];
