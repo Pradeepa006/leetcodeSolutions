07f8e4838871330a4404d1431449cb87a27a6769
Pradeepa006 <155996905+Pradeepa006@users.noreply.github.com>
2025-06-12 11:57:15 +0530

    Time: 4 ms (100%), Space: 44.4 MB (18.49%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,10 +1,9 @@
 class Solution {
     public boolean isPalindrome(int x) {
+
         if(x < 0)
-        {
             return false;
-        }
-        
+    
         int copy = x ;
         int rev = 0;
 
@@ -16,11 +15,8 @@ class Solution {
         }
 
         if( rev == copy)
-        {
             return true;
-        }
 
         return false ;
-        
     }
 }
