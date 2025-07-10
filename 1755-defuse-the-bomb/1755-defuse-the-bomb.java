ad3c34ce463609df7978cb9349d026028c745313
Pradeepa006 <155996905+Pradeepa006@users.noreply.github.com>
2025-07-10 10:44:24 +0530

    Time: 0 ms (100%), Space: 42.6 MB (23.06%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.java
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -7,20 +7,50 @@ class Solution {
             return arr;
         }
 
-        for(int i = 0 ; i < n ; i++){
-            int sum = 0;
-            if(k > 0){
-                for(int j = 1 ; j <= k ; j++){
-                    sum = sum + code[(i + j) % n];
-                }
-            }
-            if( k < 0){
-                for(int j = -1 ; j >= k ; j--){
-                    sum = sum + code[(i + j + n) % n];
-                }
-            }
-            arr[i] = sum;
+        int[] cirArr = new int[2 * n];
+
+        for(int i = 0 ; i < 2 * n ; i++)
+        {
+            cirArr[i] = code[i % n];
         }
+
+        int start , end;
+
+        if (k > 0){
+            start = 1;
+            end = k;
+        }
+        else{
+            start = n + k;
+            end = n - 1;
+        }
+        int sum = 0;
+        
+
+        for(int i = start ; i <= end ; i++){
+            sum = sum + cirArr[i];
+        }
+        
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = sum;
+            sum = sum + cirArr[i + end + 1 ];
+            sum = sum - cirArr[start + i];
+        }
+
+        // for(int i = 0 ; i < n ; i++){
+        //     int sum = 0;
+        //     if(k > 0){
+        //         for(int j = 1 ; j <= k ; j++){
+        //             sum = sum + code[(i + j) % n];
+        //         }
+        //     }
+        //     if( k < 0){
+        //         for(int j = -1 ; j >= k ; j--){
+        //             sum = sum + code[(i + j + n) % n];
+        //         }
+        //     }
+        //     arr[i] = sum;
+        // }
         return arr;
         
     }
